ca6ff7a12b5223c43806667a4d09b9f4f98386af
whr <whr@whr.local>
2020-04-19 19:07:10 +0800

    whr

--- src/map-demo.rs
+++ src/map-demo.rs
@@ -6,7 +6,7 @@ fn map_demo() {
         let mut m = HashMap::new();
         m.insert(String::from("a"), 10);
         m.insert(String::from("b"), 20);
-        m.insert(String::from("c"), 30);
+        m.entry(String::from("c")).or_insert(30);
 
         for (k, v) in &m {
             println!("----------------------");
@@ -14,5 +14,8 @@ fn map_demo() {
             println!(" key: {},value: {}", k, v);
             println!(" key: {},value: {}", k, v);
         }
+        println!("------map----------------");
+        println!("{:?}", m);
     }
+    {}
 }
