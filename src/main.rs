15ca7567639471a39d9b5440ad8cc3b557812de3
whr <whr@whr.local>
2020-04-22 10:18:36 +0800

    whr

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::borrow::{Borrow, BorrowMut};
 use std::ops::Add;
+
 include!("t01-print-demo.rs");
 include!("t02-slice-str-demo.rs");
 include!("t03-map-demo.rs");
 include!("t04-vec-demo.rs");
 include!("t05-ownership-demo.rs");
+include!("t07-struct.rs");
 
 
 fn main() {
@@ -15,6 +17,7 @@ fn main() {
     ownership_demo();
     //
     vec_test();
+    test_struct();
 }
 
 
