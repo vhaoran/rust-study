b40250d41ec1c13569cde53f6d208d35423abc44
whr <whr@w520>
2020-05-20 21:50:37 +0800

    aaaa

--- src/t01/t_fn.rs
+++ src/t01/t_fn.rs
@@ -7,6 +7,10 @@ fn test_fn_1() {
     let mut b: i32 = 44;
     fn_1(&mut a, &mut b);
     println!("a: {} b: {}", a, b);
+
+    fn_2(&mut a, &mut b);
+    println!("-=-------------------------------");
+    println!("a: {} b: {}", a, b);
 }
 
 fn fn_1(a: &mut i32, b: &mut i32) {
@@ -14,3 +18,11 @@ fn fn_1(a: &mut i32, b: &mut i32) {
     *a = *b;
     *b = t
 }
+
+fn fn_2(a: &mut i32, b: &mut i32) {
+    //(*a, *b) = (*b, *a);
+    let (aa, bb) = (*a, *b);
+    *a = bb;
+    *b = aa;
+}
+
---
+++ src/t06_struct/mod.rs
@@ -0,0 +1,2 @@
+
+mod t_struct;
