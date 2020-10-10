72a75d40f18721c63077d11d1fe30422b4ac9ec5
whr <whr@whr.local>
2020-10-10 18:19:25 +0800

    aaaa

--- src/t14_log/mod.rs
+++ src/t14_log/mod.rs
@@ -1,1 +1,13 @@
-mod log_1;
+mod simplelog_1;
+
+
+pub fn test_log() {
+    use log::*;
+    // use simplelog::*;
+
+    error!("----------Bright red error");
+    error!("----------Bright red error");
+
+    info!("----------This only appears in the log file");
+    info!("----------This only appears in the log file");
+}
---
+++ src/t14_log/simplelog_1.rs
@@ -0,0 +1,39 @@
+extern crate log;
+extern crate simplelog;
+
+
+use log::*;
+use simplelog::*;
+
+use std::fs::File;
+
+#[test]
+fn simple_log_1() {
+    fn x() {
+        CombinedLogger::init(vec![
+            // #[cfg(feature = "termcolor")]
+            //     TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
+
+            TermLogger::new(LevelFilter::Info,
+                            Config::default(),
+                            TerminalMode::Mixed),
+            WriteLogger::new(LevelFilter::Info,
+                             Config::default(),
+                             File::create("simple.log").unwrap()),
+        ])
+            .unwrap();
+
+        error!("Bright red error");
+        error!("Bright red error");
+
+        info!("This only appears in the log file");
+        info!("This only appears in the log file");
+
+        debug!("This level is currently not enabled for any logger");
+        debug!("This level is currently not enabled for any logger");
+    }
+
+    x();
+
+    super::test_log();
+}
