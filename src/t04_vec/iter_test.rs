5c3031ea9dbcdc045178890adef44130a933e57a
whr <whr@whr.local>
2021-10-18 18:28:52 +0800

    auto commit

--- src/t04_vec/iter_test.rs
+++ src/t04_vec/iter_test.rs
@@ -9,20 +9,33 @@ pub struct Data {
 
 #[test]
 fn it_1() {
-    let l:Vec<i64> = vec![1, 2, 3, 4];
+    let l: Vec<i64> = vec![1, 2, 3, 4];
 
-    let a:Vec<Data> = l.iter().map(|x| {
-        let s = format!("{}_title", x);
-        Data { id: x.clone(), title: s }
-    }).collect();
+    let a: Vec<Data> = l
+        .iter()
+        .map(|x| {
+            let s = format!("{}_title", x);
+            Data {
+                id: x.clone(),
+                title: s,
+            }
+        })
+        .collect();
 
-    println!("-----------{:?}-----------",a);
-    a.iter().for_each(|x|println!("v: {:?}",x.clone()));
+    println!("-----------{:?}-----------", a);
+    a.iter().for_each(|x| println!("v: {:?}", x.clone()));
     println!("----------------------\n\n");
-    let r = a.iter().find(|x|&x.id == &3);
-    println!("-----------r {:?}-----------",r);
-    let r = a.iter().any(|x|&x.id == &4);
-    println!("-----------r {:?}-----------",r);
+    let r = a.iter().find(|x| &x.id == &3);
+    println!("-----------r {:?}-----------", r);
+    let r = a.iter().any(|x| &x.id == &4);
+    println!("-----------r {:?}-----------", r);
+}
 
+#[test]
+fn v_m_1() {
+    //---------------------
+    let src = vec![1, 2, 3];
+    let l:Vec<_> = src.iter().map(|x| x + 10).collect();
 
+    println!("-----------{:#?}-----------", l);
 }
