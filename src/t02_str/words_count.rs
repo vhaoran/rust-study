1f5e1ad29d911ed2b79cae2f9007a3aed4c24200
whr <whr@whr.local>
2021-10-25 18:33:38 +0800

    auto commit

--- src/t02_str/words_count.rs
+++ src/t02_str/words_count.rs
@@ -19,3 +19,40 @@ fn words_max_len_1() {
 
     println!("-----------max: {}-----------", old);
 }
+
+#[test]
+fn f_1() {
+    //---------------------
+    let r = std::fs::read_dir("./").unwrap();
+    for v in r {
+        println!("--------------{:?}--------", v);
+        //
+        let p = v.unwrap();
+        println!("-----------file_name:  {:?}--------", p.file_name().clone().into_string());
+    }
+
+    if std::path::Path::new("./data").exists(){
+        println!("-----------exists-----------");
+    }else{
+        println!("---------not--exists-----------");
+    }
+
+    if std::path::Path::new("./src").exists(){
+        println!("-----------exists-----------");
+    }else{
+        println!("---------not--exists-----------");
+    }
+
+}
+
+
+fn queue_name() -> String {
+    let s = std::env::args().nth(0).unwrap();
+    s
+}
+
+#[test]
+fn app_path_1() {
+    //---------------------
+    println!("-----------{}-----------",queue_name());
+}
