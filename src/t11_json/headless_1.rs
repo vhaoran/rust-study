929f871f965bed63060227db19ccb2924532d3b5
whr <whr@whr.local>
2022-01-13 18:46:01 +0800

    auto commit

--- src/t11_json/headless_1.rs
+++ src/t11_json/headless_1.rs
@@ -9,30 +9,22 @@ async fn hl_1() -> Result<(), Box<dyn std::error::Error>> {
     println!("-----------a-----------");
 
     let tab = browser.wait_for_initial_tab()?;
+    // http://www.kuwo.cn/play_detail/150602055
 
     /// Navigate to wikipedia
-    tab.navigate_to("http://www.zgei.com/")?;
+    let url =
+        "http://www.kuwo.cn/search/list?key=%E5%90%AC%E9%97%BB%E8%BF%9C%E6%96%B9%E6%9C%89%E4%BD%A0";
+    let r = tab.navigate_to(url)?;
     println!("-----------after after navigate-----------");
+    println!("-----------{:#?}-----------",r);
+
 
     /// Wait for network/javascript/dom to make the search-box available
     /// and click it.
     // tab.wait_for_element("input#searchInput")?.click()?;
-    tab.wait_for_element("#j-input")?;
-    tab.type_str("似水流年")?;
+    // tab.wait_for_element("#j-input")?;
 
-    tab.wait_for_element("#j-submit")?;
-    println!("---after show j-submit-----");
 
-    tab.find_element("button#j-submit")?.click()?;
-    println!("----after click <j-submit>-----");
-
-    /// We should end up on the WebKit-page once navigated
-    // let r = tab.wait_until_navigated()?;
-    let r = tab.wait_for_element("#j-lrc")?;
-    let s = r.get_attributes()?;
-
-    println!("-------lrc:----{:#?}-----------", r);
-    println!("-----------after query-----------");
     tokio::time::sleep(std::time::Duration::from_secs(20)).await;
 
     Ok(())
--- src/t11_json/reqwest_1.rs
+++ src/t11_json/reqwest_1.rs
@@ -1,8 +1,11 @@
+use reqwest;
 use std::collections::HashMap;
 
 #[tokio::test]
 async fn req_1() -> Result<(), Box<dyn std::error::Error>> {
     // let url = "http://www.zgei.com";
+    let url =
+        "http://www.kuwo.cn/search/list?key=%E5%90%AC%E9%97%BB%E8%BF%9C%E6%96%B9%E6%9C%89%E4%BD%A0";
     // let resp = reqwest::Client::new()
     //     .post(url)
     //     .form(&[
@@ -13,9 +16,12 @@ async fn req_1() -> Result<(), Box<dyn std::error::Error>> {
     //     ])
     //     .send()
     //     .await?;
-    // // .json::<HashMap<String, String>>()
-    // // .await?;
-    // let text = resp.text().await?;
-    // println!("{:#?}", text);
+    // .json::<HashMap<String, String>>()
+    // .await?;
+
+    let resp = reqwest::get(url).await?;
+
+    let text = resp.text().await?;
+    println!("{:#?}", text);
     Ok(())
 }
