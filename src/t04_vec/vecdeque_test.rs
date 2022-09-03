f3247237f3df72c6d8bb5d46849fd61e56b987a7
whr <whr@whr.local>
2022-09-03 18:50:34 +0800

    auto commit

--- src/t04_vec/vecdeque_test.rs
+++ src/t04_vec/vecdeque_test.rs
@@ -3,16 +3,30 @@ use std::collections::VecDeque;
 #[test]
 fn vd_1() {
     //---------------------
-    let mut l :VecDeque<i32> = VecDeque::with_capacity(2);
-    println!("-----------capcity {}-----------",l.capacity());
+    let mut l: VecDeque<i32> = VecDeque::with_capacity(2);
+    println!("-----------capcity {}-----------", l.capacity());
 
     l.reserve(2);
-    println!("-----------capcity {}-----------",l.capacity());
+    println!("-----------capcity {}-----------", l.capacity());
 
     l.push_back(1);
     l.push_front(2);
     l.push_back(3);
-    println!("-----------{:?}-----------",l);
-    println!("-----------capcity {}-----------",l.capacity());
+    println!("-----------{:?}-----------", l);
+    println!("-----------capcity {}-----------", l.capacity());
+}
 
+#[test]
+fn fib_1() {
+    fn fib(u: u64) -> u64 {
+        match u {
+            0 => 0,
+            1 => 1,
+            _ => fib(n - 1) + fib(n - 2),
+        }
+    }
+
+    for i in 10..20_u64 {
+        println!("----------fib({i}) = {}-----------", fib(i));
+    }
 }
